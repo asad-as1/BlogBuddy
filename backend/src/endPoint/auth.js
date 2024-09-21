78953daa0c9c63e116de75e91699cb0f3fe78500
Mohd Asad Ansari <asad.as9458@gmail.com>
2024-09-21 21:59:31 +0530

    fhudfh

--- backend/src/endPoint/auth.js
+++ backend/src/endPoint/auth.js
@@ -1,6 +1,4 @@
-// controllers/authController.js
-const isAuthenticated = require('../middlewares/auth');
-
+// src/controllers/authController.js
 exports.checkAuth = (req, res) => {
   res.status(200).json({ message: 'Authenticated', user: req.user });
 };
